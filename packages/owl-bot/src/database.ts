9a57d26471156d83ef1af344f090f7d5d5057fdc
Jeffrey Rennie <rennie@google.com>
2021-02-03 18:18:32 -0800

    WIP

--- packages/owl-bot/src/database.ts
+++ packages/owl-bot/src/database.ts
@@ -12,5 +12,51 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import * as firebase from "firebase-admin";
+import admin from "firebase-admin";
+import { OwlBotLock, OwlBotYaml } from "./config-files";
 
+export interface Configs {
+  // The body of .Owlbot.lock.yaml.
+  lock: OwlBotLock | undefined,
+  // The body of .Owlbot.yaml.
+  yaml: OwlBotYaml | undefined,
+  // The commit hash from which the config files were retrieved.
+  commithash: string
+};
+
+type Db = admin.firestore.Firestore;
+
+const YAMLS = 'owl-bot-yamls';
+
+export async function getConfigs(db: Db, repo: string): Promise<Configs | undefined> {
+  const docRef = db.collection(YAMLS).doc(repo);
+  const doc = await docRef.get();
+  // Should we verify the data?
+  return doc.data() as Configs;
+}
+
+
+// Returns true if the store succeeded.
+// Returns false if replaceCommithash differed from the commithash in the
+// db, and therefore nothing was stored.
+export async function storeConfigs(db: Db, repo: string, configs: Configs,
+  replaceCommithash: string | null): Promise<boolean> {
+  const docRef = db.collection(YAMLS).doc(repo);
+  let updatedDoc = false;
+  await db.runTransaction(async (t) => {
+    const doc = await t.get(docRef);
+    const prevConfigs = doc.data() as Configs | undefined;
+    if ((prevConfigs && prevConfigs.commithash == replaceCommithash) ||
+      (!prevConfigs && replaceCommithash === null)) {
+      t.update(docRef, configs);
+      updatedDoc = true;
+    }
+  });
+  return updatedDoc;
+}
+
+export async function findReposWithPostProcessor(db: Db, dockerImageName: string): Promise<string[]> {
+  const ref = db.collection(YAMLS);
+  const got = await ref.where('yaml.docker.image', '==', dockerImageName).get();
+  return got.docs.map(doc => doc.id);
+}  
