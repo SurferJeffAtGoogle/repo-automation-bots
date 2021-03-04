cb75fecc53ff08da858621a725ca377aab9e4eab
Jeffrey Rennie <rennie@google.com>
2021-03-04 15:57:43 -0800

    WIP

--- packages/owl-bot/test/scan-googleapis-gen-and-create-pull-requests.ts
+++ packages/owl-bot/test/scan-googleapis-gen-and-create-pull-requests.ts
@@ -12,13 +12,41 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import tmp from 'tmp';
 import * as assert from 'assert';
 import {describe, it, afterEach} from 'mocha';
 import { scanGoogleapisGenAndCreatePullRequests } from '../src/scan-googleapis-gen-and-create-pull-requests';
+import { newCmd } from '../src/copy-code';
+import { makeDirTree } from './dir-tree';
+import { FakeConfigsStore } from './fake-configs-store';
+import { OctokitParams } from '../src/octokit-util';
 
 describe('scanGoogleapisGenAndCreatePullRequests', () => {
     it('works', async () => {
-        assert.ok(true);
-        // await scanGoogleapisGenAndCreatePullRequests()
+        // Create a git repo.        
+        const dir = tmp.dirSync().name;
+        const cmd = newCmd();
+        cmd('git init', {cwd: dir});
+
+        // Add 3 commits
+        makeDirTree(dir, ["a.txt:1"]);
+        cmd('git add -A', {cwd: dir});
+        cmd('git commit -m a', {cwd: dir});
+    
+        makeDirTree(dir, ["b.txt:2"]);
+        cmd('git add -A', {cwd: dir});
+        cmd('git commit -m b', {cwd: dir});
+
+        makeDirTree(dir, ["c.txt:3"]);
+        cmd('git add -A', {cwd: dir});
+        cmd('git commit -m c', {cwd: dir});
+
+        const configsStore = new FakeConfigsStore();
+        const octokitParams: OctokitParams = {
+            "app-id": 4,
+            "installation": 5,
+            "pem-path": "/no/where"
+        };
+        await scanGoogleapisGenAndCreatePullRequests(dir, octokitParams, configsStore);
     })
 });
